d8d6d00bad3c167a64709d61d43bf1f62dab313b
jmmbarbosa <j1000tonm@gmail.com>
2024-10-04 23:14:50 -0300

    Criando módulos

---
+++ Modulos/Criando_Modulo/main.py
@@ -0,0 +1,7 @@
+import operacoes as op
+
+op.somar(2,4)
+op.subtrair(4,2)
+op.dividir(8,4)
+op.multiplicar(4,2)
+op.imprimir("Uma mensagem qualquer...")
---
+++ Modulos/Criando_Modulo/operacoes.py
@@ -0,0 +1,19 @@
+"""
+    Este módulo fornece funçöes de operaçöes matemáticas básicas, como por exemplo:
+somar, subtrair, dividir e multiplicar. Ele ainda também, imprime variáveis.
+"""
+
+def somar(a, b):
+    print(a + b)
+    
+def subtrair(a, b):
+    print(a - b)
+
+def dividir(a, b):
+    print(a / b)
+    
+def multiplicar(a, b):
+    print(a * b)
+
+def imprimir(val):
+    print(val)
