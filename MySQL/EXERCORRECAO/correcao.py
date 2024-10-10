a5d4a9178076774f907783b198a8ae0823e9038d
jmmbarbosa <j1000tonm@gmail.com>
2024-10-09 22:22:52 -0300

    Correção insert_data

--- MySQL/EXERCORRECAO/correcao.py
+++ MySQL/EXERCORRECAO/correcao.py
@@ -22,5 +22,20 @@ def create_table():
     sql = "CREATE TABLE alunos (id INT AUTO_INCREMENT PRIMARY KEY, nome VARCHAR(255), matricula VARCHAR(50), turma VARCHAR(50))"
     mycursor.execute(sql)
     print("Tabela criada com sucesso!")
-create_table()
+#create_table()
 
+def insert_data():
+    sql = "INSERT INTO alunos (nome, matricula, turma) VALUES (%s, %s, %s)"
+    val = [
+        ('José Lima', 'MAT90551', 'BCW22'),
+        ('Carlos Augusto', 'MAT90552', 'BCW22'),
+        ('Lívia Lima', 'MAT90553', 'BCW22'),
+        ('Sandra Gomes', 'MAT90554','BCW23'),
+        ('João Augusto', 'MAT90555','BCW23'),
+        ('Breno Lima', 'MAT90556','BCW24'),
+        ('José Vinícius', 'MAT90557', 'BCW25')
+    ]
+    mycursor.executemany(sql,val)
+    mydb.commit()
+    print(mycursor.rowcount, "linha(s) alterada(s)!")
+insert_data()
