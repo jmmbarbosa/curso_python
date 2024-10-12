0c1f0a7cdef3d95feb186e8d65951b8be1f765fd
jmmbarbosa <j1000tonm@gmail.com>
2024-10-12 00:01:42 -0300

    Importando CSV com pandas

---
+++ PANDAS/IMPORTA_CSV/main.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+
+df_csv = pd.read_csv("E:\SoulCode\PythonOO\curso_python\PANDAS\IMPORTA_CSV\matriculas.csv",sep=";")
+print(df_csv)
+#print(df_csv.head(3))
+#print(df_csv.tail(3))
